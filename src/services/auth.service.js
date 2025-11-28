901f0429a131f6dcd3eb5b8060934cdae7bf62ea
Tính Trần <tranleductinh@gmail.com>
2025-11-28 13:07:23 +0700

    Add role to user object in sign-up response

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -17,6 +17,7 @@ export const signUp = async ({ email, name, password }) => {
         id: user._id,
         name: user.name,
         email: user.email,
+        role: user.role
       },
       token,
     };
